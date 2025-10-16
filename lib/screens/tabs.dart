bbe041a84717a94beeb43c38b357c8a752b7a11d
Yolanda <yolanda@qwix.net>
2025-10-16 17:19:03 +0200

    code opt

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -25,8 +25,6 @@ class _TabsScreen extends State<TabsScreen> {
     ).showSnackBar(SnackBar(content: Text(message)));
   }
 
-  void _newfunction(String test) {}
-
   void _toggleMealFavoriteStatus(Meal meal) {
     final existingMeal = _favoriteMeals.contains(meal);
 
